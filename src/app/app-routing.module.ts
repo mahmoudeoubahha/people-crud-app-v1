130e160ba96eabe0abfebab4390dbb663730a3ab
mhamed <mahmoudemeknes@gmail.com>
2020-11-28 13:17:41 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { PeopleListComponent } from './people-list/people-list.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component:LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'people-list',
+    component: PeopleListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
